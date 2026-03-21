a2a9926477dfd3baa866093396bbd66a797e5248
Soumik <smik1172@gmail.com>
2026-03-22 00:44:56 +0530

    feat: added react router to navigate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
-        <div>
-			<HomePage />
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,26 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
     return (
-        <footer className="bg-surface-container-lowest w-full py-12 px-8 border-t border-outline-variant/10">
+        <footer className="bg-surface-container-lowest w-full py-6 px-8 border-t border-outline-variant/10">
             <div className="flex flex-col md:flex-row justify-between items-center gap-6 max-w-7xl mx-auto">
                 <div className="text-lg font-bold text-on-surface">
                     FactTube
                 </div>
-                <p className="font-mono text-[10px] uppercase tracking-widest text-tertiary-container">
-                    © {new Date().getFullYear()} FactTube.
+                <p className="font-mono text-xs uppercase tracking-widest text-tertiary-container">
+                    © {new Date().getFullYear()} FactTube
                 </p>
                 <div className="flex gap-8">
-                    <a
-                        href="#"
+                    <Link
+                        to="https://github.com/Smiky0/Factube/blob/main/LICENSE"
+                        target="_blank"
+                        rel="noreferrer"
                         className="font-mono text-[10px] uppercase tracking-widest text-tertiary-container hover:text-primary transition-colors"
                     >
-                        Privacy Policy
-                    </a>
-                    <a
-                        href="#"
-                        className="font-mono text-[10px] uppercase tracking-widest text-tertiary-container hover:text-primary transition-colors"
-                    >
-                        Terms of Service
-                    </a>
+                        LICENSE
+                    </Link>
                 </div>
             </div>
         </footer>
